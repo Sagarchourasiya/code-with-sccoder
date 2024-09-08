c9a530c13c475ba23deebc0bc7fd32048c6b4081
sagar chourasiya <sagar.chourasiya2002@gmail.com>
2024-09-08 15:01:01 +0530

    Add Initial File 1

---
+++ reverseinteger1.py
@@ -0,0 +1,9 @@
+# program to reverse integers 
+num = int(input("Enter a number :"))
+temp = num
+reverse = 0 
+while num > 0:
+    remainder = num % 10
+    reverse = (reverse * 10) + remainder
+    num = num // 10
+print(reverse)
